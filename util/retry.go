ff50f6edd9e7d873758a8ba4e585c5eaf627ac3a
Brandsma <a.brandsma@protonmail.com>
2020-05-15 22:58:40 +0200

    Added dependencies

--- util/retry.go
+++ util/retry.go
@@ -1,7 +1,12 @@
 package util
 
-func retry(attempts int, sleep time.Duration, fn func(*interface{}) error) error {
-	if err := fn(); err != nil {
+
+import (
+	"time"
+)
+
+func retry(attempts int, sleep time.Duration, fn func(var *interface{}) error) error {
+	if err := fn(var); err != nil {
 		if s, ok := err.(stop); ok {
 			// Return the original error for later checking
 			return s.error
@@ -9,7 +14,7 @@ func retry(attempts int, sleep time.Duration, fn func(*interface{}) error) error
 
 		if attempts--; attempts > 0 {
 			time.Sleep(sleep)
-			return retry(attempts, 2*sleep, fn)
+			return retry(attempts, 2*sleep, fn(var))
 		}
 
 		return err
