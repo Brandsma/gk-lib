6fca20fbac0ffa974a63914a0dfcf3106b5bad39
Brandsma <a.brandsma@protonmail.com>
2020-05-21 21:42:14 +0200

    Made library obsolete

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# gk-lib
+# This library is now OBSOLETE
+
+## gk-lib
 
 This is a small library used for a personal private project.
+
+
--- models/group.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// Group is the struct that determines what a group looks like
-type Group struct {
-	GroupID    string   `json:"groupId,omitempty" bson:"groupId,omitempty"`
-	GroupName  string   `json:"groupName" bson:"groupName"`
-	UserIDs    []string `json:"userIds" bson:"userIds"`
-	InvitedIDs []string `json:"invitedIds" bson:"invitedIds"`
-}
